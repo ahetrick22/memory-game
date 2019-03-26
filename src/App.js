481b9add6f23f232429cd47dc90ee95b6182d600
Alicia Hetrick <ahetrick22@gmail.com>
2019-03-26 14:24:58 -0400

    initial setup to fetch images from API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    cats: true,
+    gridSize: 16,
+    imgArr: []
+  }
+
+  componentDidMount = async () => {
+    if (this.state.cats) {
+      for (let i = 0; i < this.state.gridSize/2; i++) {
+        await fetch("https://api.thecatapi.com/v1/images/search",{
+          headers: {
+            "x-api-key": "693e02ea-6fbe-4cd0-a6da-ec89ba26d2e7",
+            "Content-Type": "application/json"
+          }, mode: "cors"
+        }).then(res => res.json())
+        .then(jsonRes => {
+          console.log(jsonRes[0].url)
+          this.setState( {imgArr : this.state.imgArr.concat(jsonRes[0].url)} );
+        }) 
+      }
+    }
+  }
+
+  buildCards = () => {
+    return this.state.imgArr.map((imgURL, index) => <div key={index} className="card"><img src={imgURL} alt="cat" /></div>)
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+      <h1>Pet Memory</h1>
+      {this.buildCards()}
+    </>
     );
   }
 }
+
+
 
 export default App;
