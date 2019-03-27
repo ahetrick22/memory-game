e000df943b158d83f2ec06b78eae6a4c31a85f9d
Alicia Hetrick <ahetrick22@gmail.com>
2019-03-26 21:26:47 -0400

    get random cat pictures, double them, and shuffle the board

--- src/App.js
+++ src/App.js
@@ -11,24 +11,40 @@ class App extends Component {
 
   componentDidMount = async () => {
     if (this.state.cats) {
-      for (let i = 0; i < this.state.gridSize/2; i++) {
-        await fetch("https://api.thecatapi.com/v1/images/search",{
-          headers: {
-            "x-api-key": "693e02ea-6fbe-4cd0-a6da-ec89ba26d2e7",
-            "Content-Type": "application/json"
-          }, mode: "cors"
-        }).then(res => res.json())
-        .then(jsonRes => {
-          console.log(jsonRes[0].url)
-          this.setState( {imgArr : this.state.imgArr.concat(jsonRes[0].url)} );
-        }) 
-      }
+      await this.fetchCats();
+      console.log(this.state.imgArr);
+    }
+    const doubledArr = [...this.state.imgArr];
+    this.setState( {imgArr: this.state.imgArr.concat(doubledArr) });
+    console.log(this.state.imgArr);
+  }
+
+  fetchCats = async () => {
+    for (let i = 0; i < this.state.gridSize/2; i++) {
+      await fetch("https://api.thecatapi.com/v1/images/search",{
+        headers: {
+          "x-api-key": "693e02ea-6fbe-4cd0-a6da-ec89ba26d2e7",
+          "Content-Type": "application/json"
+        }, mode: "cors"
+      }).then(res => res.json())
+      .then(jsonRes => {
+        console.log(jsonRes[0].url)
+        this.setState( {imgArr : this.state.imgArr.concat(jsonRes[0].url)} );
+      }) 
     }
   }
 
   buildCards = () => {
-    return this.state.imgArr.map((imgURL, index) => <div key={index} className="card"><img src={imgURL} alt="cat" /></div>)
+    this.shuffleArray(this.state.imgArr);
+    return this.state.imgArr.map((imgURL, index) => <span key={index}><img className="card tile" src={imgURL} alt="cat" /></span>)
   }
+
+  shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
   
   render() {
     return (
